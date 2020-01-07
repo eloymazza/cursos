d5a78fb9996d477e03067bede1c33ffe0d86b5fc
Eloy Mazza <eloymazzza.ra@gmail.com>
2020-01-07 04:06:03 -0300

    Added basics-test-2 for angular courses

--- angular/first-ang-app/src/app/servers/servers.component.ts
+++ angular/first-ang-app/src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -17,5 +19,13 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Server name: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value; 
+  } 
 
 }
