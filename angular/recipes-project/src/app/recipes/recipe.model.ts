64bb69ee494a131bc4a341f91411702463a0cf34
Eloy Mazza <eloymazza.ra@gmail.com>
2020-01-19 21:16:28 -0300

    12%

--- angular/recipes-project/src/app/recipes/recipe.model.ts
+++ angular/recipes-project/src/app/recipes/recipe.model.ts
@@ -5,7 +5,7 @@ export class Recipe {
     
     constructor(name: string, desc: string, imagePath: string){
         this.name = name;
-        this.description = this.description;
+        this.description = desc;
         this.imagePath = imagePath;
     }
 }
